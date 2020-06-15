d8eed8dad8e71e794388a80e9e1fc7e105834dde
BotHunterr <kumardotel18@gmail.com>
2020-06-15 11:57:12 +0545

    afkasdfaj

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// $(window).scroll(function(){
+//     if ($(this).scrollTop()>800) {
+//         $(".navbar").addClass("red")
+//     }
+//     else {
+//         $(".navbar").removeClass("red")
+//     }
+// })
